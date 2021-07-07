686bba4e1930658c9548ae4ac3aa647e963c035d
Emile Conde <condemile10@gmail.com>
2021-07-07 16:46:56 +0000

    Adding the GUI

---
+++ apikey.py
@@ -0,0 +1,2 @@
+def get_api_key():
+    return '8c10b92910a94c04954844719ea65bb2'
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
 from apikey import get_api_key
 
-query = '?ingredients=apples,+flour,+sugar'
+query = 'ingredients=apples,+flour,+sugar'
 api_key = '&apiKey='+get_api_key()
-number_of_result = '&number=10'
+number_of_result = '&number=2'
 ending = 'findByIngredients?'
 # base_url = f'https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2&apiKey={api_key}'
 base_url = 'https://api.spoonacular.com/recipes/'+ending+query+number_of_result+api_key
